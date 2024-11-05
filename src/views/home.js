fda12b592fe800c2ff933c89bf63b69d38590416
ilhan akkuş <ilhanakkus@gmail.com>
2024-11-06 00:12:05 +0300

    Add files via upload

---
+++ src/views/home.js
@@ -0,0 +1,211 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container1">
+      <Helmet>
+        <title>Presentation Page</title>
+        <meta property="og:title" content="Presentation Page" />
+      </Helmet>
+      <div data-role="Header" className="home-navbar-container">
+        <div className="home-navbar">
+          <div className="home-logo"></div>
+          <img src="/mioptik-400w.png" alt="image" className="home-image1" />
+          <div className="home-cta-container">
+            <div data-role="BurgerMenu" className="home-burger-menu">
+              <svg viewBox="0 0 1024 1024" className="home-icon10">
+                <path d="M128 256h768v86h-768v-86zM128 554v-84h768v84h-768zM128 768v-86h768v86h-768z"></path>
+              </svg>
+            </div>
+          </div>
+          <div data-role="MobileMenu" className="home-mobile-menu">
+            <div className="home-top">
+              <img alt="image" src="/default-img.svg" className="home-image2" />
+              <div data-role="CloseMobileMenu" className="home-container2">
+                <svg viewBox="0 0 1024 1024" className="home-icon12">
+                  <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                </svg>
+              </div>
+            </div>
+            <div className="home-mid">
+              <div className="home-links-container">
+                <a href="#features" className="home-link1 Anchor">
+                  features
+                </a>
+                <span className="home-link2 Anchor">services</span>
+                <span className="home-link3 Anchor">About Us</span>
+                <a href="#contact" className="home-link4 Anchor">
+                  contact
+                </a>
+              </div>
+              <button className="home-cta-btn Anchor button">
+                sTART BUILDING
+              </button>
+            </div>
+            <div className="home-bot">
+              <div className="home-social-links-container1">
+                <svg
+                  viewBox="0 0 950.8571428571428 1024"
+                  className="home-icon14"
+                >
+                  <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+                </svg>
+                <svg
+                  viewBox="0 0 877.7142857142857 1024"
+                  className="home-icon16"
+                >
+                  <path d="M713.143 73.143c90.857 0 164.571 73.714 164.571 164.571v548.571c0 90.857-73.714 164.571-164.571 164.571h-107.429v-340h113.714l17.143-132.571h-130.857v-84.571c0-38.286 10.286-64 65.714-64l69.714-0.571v-118.286c-12-1.714-53.714-5.143-101.714-5.143-101.143 0-170.857 61.714-170.857 174.857v97.714h-114.286v132.571h114.286v340h-304c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571z"></path>
+                </svg>
+                <svg
+                  viewBox="0 0 877.7142857142857 1024"
+                  className="home-icon18"
+                >
+                  <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-hero">
+        <div className="home-hero-text-container">
+          <h1 className="home-heading1">BİZ KİMİZ</h1>
+          <span className="home-text10 Section-Text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam.c Sed nisi.
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+            massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+            sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos.
+          </span>
+        </div>
+        <img
+          alt="image"
+          src="/mioptik_magaza-1500w.jpeg"
+          className="home-image3"
+        />
+      </div>
+      <div className="home-section-separator1"></div>
+      <div id="features" className="home-features">
+        <div className="home-heading-container">
+          <h2 className="home-text11 Section-Heading">BAZI ÜRÜNLERİMİZ</h2>
+          <span className="home-text12 Section-Text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam.
+          </span>
+        </div>
+        <div className="home-cards-container">
+          <div className="home-feature-card1">
+            <img
+              alt="image"
+              src="https://images.unsplash.com/photo-1619548683455-23b17c3ddc30?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;h=1000"
+              className="home-image4"
+            />
+            <span className="home-heading2 Card-Heading">Why</span>
+            <span className="home-text13 Card-Text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam.c Sed
+              nisi. Nulla quis sem at nibh elementum imperdiet.
+            </span>
+          </div>
+          <div className="home-feature-card2">
+            <img
+              alt="image"
+              src="https://images.unsplash.com/photo-1619547871672-b6562e042c1e?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;h=1000"
+              className="home-image5"
+            />
+            <span className="home-heading3 Card-Heading">What</span>
+            <span className="home-text14 Card-Text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam.c Sed
+              nisi. Nulla quis sem at nibh elementum imperdiet.
+            </span>
+          </div>
+          <div className="home-feature-card3">
+            <img
+              alt="image"
+              src="https://images.unsplash.com/photo-1619555241737-9c364cf1fbce?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;h=1000"
+              className="home-image6"
+            />
+            <span className="home-heading4 Card-Heading">How</span>
+            <span className="home-text15 Card-Text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam.c Sed
+              nisi. Nulla quis sem at nibh elementum imperdiet.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="home-section-separator2"></div>
+      <div id="contact" className="home-contact">
+        <div className="home-content-container">
+          <h2 className="home-text16 Section-Heading">BİZE ULAŞIN</h2>
+          <div className="home-locations-container">
+            <div className="home-location1">
+              <span className="home-heading5">Bahçelievler Mağaza</span>
+              <div className="home-adress">
+                <svg viewBox="0 0 1024 1024" className="home-icon20">
+                  <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                </svg>
+                <span className="Section-Text">
+                  Bahçelievler Mah. Fevzi Çamka Caddesi No:30/b
+                  Bahçelievler/İstanbul
+                </span>
+              </div>
+              <div className="home-email">
+                <svg viewBox="0 0 1024 1024" className="home-icon22">
+                  <path d="M854 342v-86l-342 214-342-214v86l342 212zM854 170q34 0 59 26t25 60v512q0 34-25 60t-59 26h-684q-34 0-59-26t-25-60v-512q0-34 25-60t59-26h684z"></path>
+                </svg>
+                <span className="Section-Text">mioptik.lens@gmail.com</span>
+              </div>
+              <div className="home-phone">
+                <svg
+                  viewBox="0 0 804.5714285714286 1024"
+                  className="home-icon24"
+                >
+                  <path d="M804.571 708.571c0 20.571-9.143 60.571-17.714 79.429-12 28-44 46.286-69.714 60.571-33.714 18.286-68 29.143-106.286 29.143-53.143 0-101.143-21.714-149.714-39.429-34.857-12.571-68.571-28-100-47.429-97.143-60-214.286-177.143-274.286-274.286-19.429-31.429-34.857-65.143-47.429-100-17.714-48.571-39.429-96.571-39.429-149.714 0-38.286 10.857-72.571 29.143-106.286 14.286-25.714 32.571-57.714 60.571-69.714 18.857-8.571 58.857-17.714 79.429-17.714 4 0 8 0 12 1.714 12 4 24.571 32 30.286 43.429 18.286 32.571 36 65.714 54.857 97.714 9.143 14.857 26.286 33.143 26.286 50.857 0 34.857-103.429 85.714-103.429 116.571 0 15.429 14.286 35.429 22.286 49.143 57.714 104 129.714 176 233.714 233.714 13.714 8 33.714 22.286 49.143 22.286 30.857 0 81.714-103.429 116.571-103.429 17.714 0 36 17.143 50.857 26.286 32 18.857 65.143 36.571 97.714 54.857 11.429 5.714 39.429 18.286 43.429 30.286 1.714 4 1.714 8 1.714 12z"></path>
+                </svg>
+                <span className="Section-Text">Phone Number</span>
+              </div>
+            </div>
+          </div>
+          <div className="home-social-links-container2">
+            <svg
+              viewBox="0 0 950.8571428571428 1024"
+              id="tw"
+              className="home-icon26"
+            >
+              <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+            </svg>
+            <svg
+              viewBox="0 0 877.7142857142857 1024"
+              id="face"
+              className="home-icon28"
+            >
+              <path d="M713.143 73.143c90.857 0 164.571 73.714 164.571 164.571v548.571c0 90.857-73.714 164.571-164.571 164.571h-107.429v-340h113.714l17.143-132.571h-130.857v-84.571c0-38.286 10.286-64 65.714-64l69.714-0.571v-118.286c-12-1.714-53.714-5.143-101.714-5.143-101.143 0-170.857 61.714-170.857 174.857v97.714h-114.286v132.571h114.286v340h-304c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571z"></path>
+            </svg>
+            <svg
+              viewBox="0 0 877.7142857142857 1024"
+              id="inst"
+              className="home-icon30"
+            >
+              <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+            </svg>
+          </div>
+        </div>
+        <img
+          alt="image"
+          src="/mioptik_magaza-1500w.jpeg"
+          className="home-image7"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Home
